01546b1762181b8e2984056b2fb64bccc5554428
Baofeng Guo <peterguo1983@gmail.com>
2022-01-23 14:11:49 -0800

    Added more questions into the index.js.

--- index.js
+++ index.js
@@ -7,7 +7,40 @@ const generateReadme = require('./utils/generateMarkdown.js');
 // Create an array of questions for user input
 const questions = () => {
     // using inquirer to prompt questions to user 
-    return inquirer.prompt([{
+    return inquirer.prompt([
+        {
+            type: "input",
+            name: "projectTitle",
+            message: "What is the project title?",
+            validate: function (answer) {
+                if (answer.length < 1) {
+                    return console.log("Must enter a valid project title.");
+                }
+                return true;
+            }
+        },
+        {
+            type: "input",
+            name: "description",
+            message: "Write a brief description of your project: ",
+            validate: function (answer) {
+                if (answer.length < 1) {
+                    return console.log("A valid project description is required.");
+                }
+                return true;
+            }
+        },
+        {
+            type: "input",
+            name: "installation",
+            message: "Describe the installation process if any: ",
+        },
+        {
+            type: "input",
+            name: "usage",
+            message: "What is this project usage for?"
+        },
+        {
         type: 'checkbox', 
         name: 'license',
         choices: ["MIT", "GPLv3", "GPL"],
